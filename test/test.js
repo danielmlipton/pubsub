65d4a6e6516ea08d6448b7453724eb7452f204c5
Daniel M. Lipton <daniel@mlipton.com>
2012-12-30 13:51:50 -0500

    Update test/test.js
    Removed some commented out cruft.

--- test/test.js
+++ test/test.js
@@ -46,37 +46,3 @@ test( "publish, one subscriber", function() {
     );
 
 });
-
-// test( "publish, two subscribes", function() {
-
-// 	expect( 4 );
-
-// 	var tokenA = _pubsub.subscribe({
-// 		topic: 'foo',
-// 		callback: function () { return }
-// 	});
-
-// 	var tokenB = _pubsub.subscribe({
-// 		topic: 'foo',
-// 		callback: function () { return }
-// 	});
-
-// 	equal( typeof token, "string", "token is string" );
-
-// 	var subscribers = _pubsub.getSubscribers( 'foo' );
-// 	equal( subscribers.length, 1, "subscribers.length is 1" );
-// 	equal( _pubsub.publish( 'foo' ), true, "_pubsub.publish( 'foo' )" );
-//     throws( _pubsub.publish, Error, "_pubsub.publish()" );
-
-// });
-
-// asyncTest( "clock one second later", function() {
-
-//   expect( 1 );
- 
-//   setTimeout(function() {
-//     ok( _date.getTime() <= _clock.epoch + 1100, "Time in milliseconds is one second-ish later" );
-//     start();
-//   }, 1010);
-
-// });
