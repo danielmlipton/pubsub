9a371c65d7eb9c2f980a70f072ea772fb12002ef
Daniel M. Lipton <daniel@mlipton.com>
2012-12-31 00:59:22 -0500

    That didn't work out as planned...
    So I didn't run my tests before committing.  Harumph.
    
    Added the submodule back for js-test-driver-phantomjs.  Removed
    test/phantomjs-jstd.js
    Added the polyfill to test/test.js
    Updated .travis.yml

--- test/test.js
+++ test/test.js
@@ -2,6 +2,34 @@ sinon.config = {
     useFakeTimers: false,
 };
 
+// WTF, PhantomJS?
+//    http://code.google.com/p/phantomjs/issues/detail?id=522
+// Polyfill from:
+//    https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+ 
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+ 
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+ 
+    return fBound;
+  };
+}
+
 QUnit.module(
 	'PubSub', {
 		setup: function () {
